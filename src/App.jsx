17aac6656c6070856b39412321d0d2fff27e95e4
Mgunara0305 <vuyelwamguni@gmail.com>
2026-02-14 11:17:27 +0200

    product details

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
+
 import './App.css'
 import AuthProvider from './context/AuthContext'
+import ProductDetails from './pages/ProductDetails'
 
 
 function App() {
@@ -16,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/auth" element={<Auth />}/>
         <Route path="/checkout" element={<Checkout />}/>
+        <Route path="/products/:id" element={<ProductDetails />}/>
       </Routes> 
     </div>
     </AuthProvider>
