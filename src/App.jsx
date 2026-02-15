3d02ed049423243716ca27e12d0659b1b1e9eaa2
Mgunara0305 <vuyelwamguni@gmail.com>
2026-02-15 16:33:26 +0200

    Finished cart and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Navbar from './components/Navbar'
 
 import './App.css'
 import AuthProvider from './context/AuthContext'
+import CartProvider from './context/CartContext'
 import ProductDetails from './pages/ProductDetails'
 
 
 function App() {
   return (
     <AuthProvider>
+      <CartProvider>
     <div className='app'>
       <Navbar />
       <Routes>
@@ -21,6 +23,7 @@ function App() {
         <Route path="/products/:id" element={<ProductDetails />}/>
       </Routes> 
     </div>
+    </CartProvider>
     </AuthProvider>
   );
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,15 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getProductById } from "../data/products";
+import { useCart } from "../context/CartContext";
+
 
 export default function ProductDetails() {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
     const navigate = useNavigate();
-
+    const {addToCart, cartItems} = useCart ();
+        
     useEffect(() => {
         const foundProduct = getProductById(id);
 
@@ -21,6 +24,12 @@ export default function ProductDetails() {
     if (!product) {
         return <h1>Loading...</h1>;
     }
+    const productInCart = cartItems.find((item) => item.id === product.id);
+    
+    const productQuantityLabel = productInCart 
+    ? `(${productInCart.quantity})`
+    : "";
+
     
     return (
     <div className="page">
@@ -33,7 +42,12 @@ export default function ProductDetails() {
                     <h1 className="product-detail-name">{product.name}</h1>
                     <p className="product-detail-price">${product.price}</p>
                     <p className="product-detail-description">{product.description}</p>
-                    <button className="btn btn-primary">Add To Cart</button>
+                    <button 
+                    className="btn btn-primary"
+                    onClick={() => addToCart(product.id)}
+                    >
+                    Add To Cart {productQuantityLabel}
+                    </button>
                 </div>
             </div>
         </div>
